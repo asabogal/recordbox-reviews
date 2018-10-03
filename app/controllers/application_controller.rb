366c18322f19cff43f1773fc31d5ff170f6a5654
asabogal <asabogal.git@gmail.com>
2018-10-03 01:45:37 -0400

    setup application_controller and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  helper_method :login!, :logged_in?, :current_user, :require_login, :authorized_user?, :logout!
+
+  def login!
+    session[:user_id] = @user.id
+  end
+
+  def logged_in?
+    !!session[:user_id]
+
+  def current_user
+    @current_user ||= User.find_by_id(id: params[:id]) if session[:user_id]
+  end
+
+  def require_login
+    redirect_to '/' if !logged_in?
+  end
+
+  def authorized_user?
+     @user == current_user
+   end
+
+   def logout!
+     session.clear
+   end
+
 end
