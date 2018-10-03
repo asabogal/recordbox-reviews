3962c3627b8ad8aebd8767a83e0adc0bd4eb4f1e
asabogal <asabogal.git@gmail.com>
2018-10-03 02:05:23 -0400

    refactor and add show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show]
+
   def new
     @user = User.new
+  end
+
+  def show
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to '/'
+      login!
+      redirect_to user_path(@user)
     else
-      #message
+      flash[:message] = "Incorrect information. Please try again!"
       redirect_to new_user_path
     end
   end
@@ -17,7 +22,13 @@ class UsersController < ApplicationController
 
   private
 
+  def find_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
+
 end
