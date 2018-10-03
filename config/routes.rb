4b745c169b521435a44bc0de5aec2e238310e15f
asabogal <asabogal.git@gmail.com>
2018-10-03 02:40:08 -0400

    setup welcome page and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  root 'welcome#home'
 
   resources :users, only: [:index, :new, :show, :create]
+  get '/users/:id/collection', to: 'users#collection', as: 'user_collection'
 
 end
