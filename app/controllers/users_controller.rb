23e4bf0848b29b874bf2e41260ab268357eccb4c
asabogal <asabogal.git@gmail.com>
2018-10-03 01:34:44 -0400

    add basic user signin functonality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      #message
+      redirect_to new_user_path
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
