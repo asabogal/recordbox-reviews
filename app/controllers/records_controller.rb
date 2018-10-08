61a4f4dbe3d801056a1ef1b0d240df76d115b355
asabogal <asabogal.git@gmail.com>
2018-10-08 15:53:48 -0400

    better create action with authentication

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,10 +1,10 @@
 class RecordsController < ApplicationController
+  before_action :find_record, only: [:show, :edit, :update]
 
   def index
   end
 
   def show
-    @record = Record.find(params[:id])
   end
 
   def new
@@ -20,8 +20,9 @@ class RecordsController < ApplicationController
 
   def create
     @record = Record.new(record_params)
+    @record.user = current_user
       if @record.save
-      redirect_to record_path(@record)
+        redirect_to record_path(@record)
     else
       flash.now[:message] = "<strong>Please try again. There were some errors:</strong><br>".html_safe + @record.errors.full_messages.join("<br/>").html_safe
       render :new
@@ -36,6 +37,10 @@ class RecordsController < ApplicationController
 
 
   private
+
+  def find_record
+    @record = Record.find(params[:id])
+  end
 
   def record_params
     params.require(:record).permit(:artist, :title, :label, :format, :cat, :release, :genre, :image, :user_id, tracks_attributes: Track.attribute_names.map(&:to_sym).push(:_destroy))
