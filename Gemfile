2d275115c462139c15c464f50233d1d69e389c85
asabogal <asabogal.git@gmail.com>
2018-10-07 23:49:41 -0400

    add cocoon gem and enable JS tags + basic record creation

--- Gemfile
+++ Gemfile
@@ -9,9 +9,24 @@ gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
+gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
+group :development, :test do
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+end
+
+group :development do
+  gem 'web-console'
+  gem 'listen', '>= 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'better_errors'
+  gem 'rails_layout'
+end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bcrypt'
 gem 'pry'
@@ -21,25 +36,12 @@ gem 'omniauth-facebook'
 gem 'dotenv-rails'
 
 gem 'bootstrap-sass'
-gem 'jquery-rails'
+gem 'simple_form'
+gem 'cocoon', '>= 1.2.11'
 
-
-
-group :development, :test do
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-end
-
-group :development do
-  gem 'web-console', '>= 3.3.0'
-  gem 'listen', '>= 3.0.5', '< 3.2'
-  gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
-end
 
 group :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   gem 'chromedriver-helper'
 end
-
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,18 +4,43 @@ class RecordsController < ApplicationController
   end
 
   def show
+    @record = Record.find(params[:id])
   end
 
   def new
+    @record = Record.new
   end
 
   def edit
   end
 
   #post/patch
+  #----
 
   def create
+    @record = Record.new(record_params.except(params[:record][:track_name]))
+
+      # @track = Track.new(name: params[:record][:track_name])
+      @record.tracks.build(name: params[:record][:track_name])
+      @record.save
+      redirect_to record_path(@record)
   end
+
+  # def create
+  #   @record = Record.new
+  #   if params[:record][:track_name].present?
+  #     # @track = Track.new(name: params[:record][:track_name])
+  #     @track = @record.tracks.build(name: params[:record][:track_name])
+  #     @record.tracks << @track
+  #     @record.save
+  #     @record.reload
+  #     flash[:message] = "Add another track?"
+  #     render :new
+  #   else
+  #     @record.save
+  #     redirect_to record_path(@record)
+  #   end
+  # end
 
   def update
   end
@@ -27,7 +52,7 @@ class RecordsController < ApplicationController
   private
 
   def record_params
-    params.require(:record).permit(:artist, :title, :label, :format, :cat, :release, :genre, :user_id)
+    params.require(:record).permit(:artist, :title, :label, :format, :cat, :release, :genre, :track_name, :user_id)
   end
 
 end
