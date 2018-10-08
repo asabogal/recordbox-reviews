54a1daa329ac79ee8bb90353efc36ae9a5388609
asabogal <asabogal.git@gmail.com>
2018-10-07 23:53:57 -0400

    improve association behaviors

--- app/models/record.rb
+++ app/models/record.rb
@@ -3,20 +3,4 @@ class Record < ApplicationRecord
   has_many :reviews
   has_many :tracks, dependent: :destroy
   accepts_nested_attributes_for :tracks, allow_destroy: true, reject_if: proc { |attributes| attributes['name'].blank? }
-
-  # def track_name(name)
-  #    self.name = Track.find_or_create_by(name: name)
-  #  end
-  #
-  #  # def track_name
-  #  #   self.track ? self.track.name : nil
-  #  # end
-  #
-  #  def track_name
-  #    if !self.tracks.empty?
-  #       self.tracks.last.name
-  #    end
-  #  end
-
-
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,3 @@
 class Track < ApplicationRecord
   belongs_to :record, optional: true
-
-
 end
