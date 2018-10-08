cfb58d0adcac2c73ee3c86025675ce470eca2284
asabogal <asabogal.git@gmail.com>
2018-10-08 15:51:41 -0400

    setup validations for user show page + setup recorbox collection view and action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show, :collection]
+  before_action :find_user, only: [:show, :recordbox]
 
   def index
   end
@@ -9,9 +9,27 @@ class UsersController < ApplicationController
   end
 
   def show
+    if authorized_user?
+      render :show
+    else
+      flash[:message] = "You don't have permision to do that!"
+      redirect_to '/'
+    end
   end
 
   def recordbox
+    @records = @user.records
+    if logged_in?
+      if authorized_user?
+        render :recordbox
+      else
+        redirect_to '/'
+      end
+    else
+      flash[:message] = "Please log in to do that"
+      redirect_to "/login"
+    end
+
   end
 
   def create
