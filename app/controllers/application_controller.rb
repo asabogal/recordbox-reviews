10a8ef676cd9678352e803d3249863ab986703d7
asabogal <asabogal.git@gmail.com>
2018-10-08 15:48:09 -0400

    fix current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find_by_id(id: params[:id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def require_login
