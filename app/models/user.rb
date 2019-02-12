939c6093ed5e75be6c751003762988976faac238
asabogal <asabogal.git@gmail.com>
2019-02-12 15:37:25 -0500

    change user username minimum requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates :username, uniqueness: true
-  validates :username, length: { minimum: 6 }
+  validates :username, length: { minimum: 4 }
   validates :email, presence: true
   validates :email, uniqueness: true
 
