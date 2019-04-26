e4ad87654f09d76d2641c80f3fbe297ad5ed5821
asabogal <asabogal.git@gmail.com>
2019-04-25 22:42:07 -0400

    add login message to unlock features

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
 
   def home
     @records = Record.rating_desc
+    if !logged_in?
+      flash[:message] = "please login to unlock all features"
+    end
   end
 
 end
